f2bd40e2bd1aba9a2a24f93a9028d236b4e8e2c3
jiyoon-lee <jiyoon3421@gmail.com>
2023-11-24 20:57:23 +0900

    231124 jyl 빌드 실패 수정완료

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import useSWR from "swr";
-import type { ApiContext, User } from "@/types";
+// import React from "react";
+// import useSWR from "swr";
+// import type { ApiContext, User } from "@/types";
 
-type AuthContextProviderProps = {
-  context: ApiContext;
-  authUser?: User;
-};
+// type AuthContextProviderProps = {
+//   context: ApiContext;
+//   authUser?: User;
+// };
 
-const AuthContextProvider = ({
-  context,
-  authUser,
-  children,
-}: React.PropsWithChildren<AuthContextProviderProps>) => {
-  const { data, error, mutate } = useSWR<User>(
-    `${context.apiRootUrl.replace(/\/$/g, "")}/users/me`
-  );
-  const isLoading = !data && !error;
+// const AuthContextProvider = ({
+//   context,
+//   authUser,
+//   children,
+// }: React.PropsWithChildren<AuthContextProviderProps>) => {
+//   const { data, error, mutate } = useSWR<User>(
+//     `${context.apiRootUrl.replace(/\/$/g, "")}/users/me`
+//   );
+//   const isLoading = !data && !error;
 
-  const signinInternal = async () => {};
-  const signoutInternal = async () => {};
-  return (
-    <AuthContext.Provider value={{ authUser: data ?? authUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   const signinInternal = async () => {};
+//   const signoutInternal = async () => {};
+//   return (
+//     <AuthContext.Provider value={{ authUser: data ?? authUser }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
 
-export default function AuthContext() {
-  return <div></div>;
-}
+// export default function AuthContext() {
+//   return <div></div>;
+// }

202b01a6766200b5d03c0dce0dfb0d5eec3dc9f3
jiyoon-lee <jiyoon3421@gmail.com>
2023-11-25 18:14:43 +0900

    231125 jyl 이미지 업로드 테스트2

--- src/components/TestTestTest.tsx
+++ src/components/TestTestTest.tsx
@@ -18,7 +18,7 @@ export default function TestTestTest() {
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "same-origin", // include, *same-origin, omit
-        headers: { "Content-Type": `multipart/form-data;boundary="boundary"` },
+        headers: { "Content-Type": "multipart/form-data" },
         body: formdata,
         redirect: "follow", // manual, *follow, error
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url

2c54938301cd44c4c0effb7c97f9248ea92c821a
jiyoon-lee <jiyoon3421@gmail.com>
2023-12-13 23:47:18 +0900

    231213 jyl 환경변수 적용안되는 오류

--- src/services/pushAlarm.ts
+++ src/services/pushAlarm.ts
@@ -4,7 +4,8 @@ import { axios } from "@/utils/axios";
 export async function pushAlarm(content: string) {
   try {
     const token = process.env.NEXT_PUBLIC_FCM_DEVICE_TOKEN;
-    const key = process.env.NENT_PUBLIC_FCM_SERVER_KEY;
+    const key =
+      "key=AAAAANVh6Cc:APA91bGmXsYiCNSKedtF_5TJhNcLTXZKi9ifM5XQ9dgft15zWP6bE10U9QslW6wNYxmsXS_4VVBrNgsGvg7teyRAR3xRVNhp3ZrdUGz72ofuL_hRv9gBDbeOdOEWsnBzJmOODGZRoHU9";
     if (token) {
       return axios.post(
         "https://fcm.googleapis.com/fcm/send",
